2e3ba54220c68897b40dc82bea4ad161229892a0
Ringo Hoffmann <riho@live.de>
2019-02-13 15:18:16 +0100

    updated circleci script and deployment

--- scripts/circleci.sh
+++ scripts/circleci.sh
@@ -1,6 +1,46 @@
 #!/bin/bash
 
+PKGNAME="github.com/zekroTJA/vplan2019"
+BUILDPATH="./bin"
+BUILDNAME="vplan2019_server"
+
+TAG=$(git describe --tags)
+if [ "$TAG" == "" ]; then
+    TAG="untagged"
+fi
+
+COMMIT=$(git rev-parse HEAD)
+
 git submodule init
 git submodule update
 
-go build -v -o ./bin/server ./cmd/server
+BUILDS=( \
+    'linux;arm' \
+    'linux;amd64' \
+    'windows;amd64' \
+    'darwin;amd64' \
+)
+
+for BUILD in ${BUILDS[*]}; do
+
+    IFS=';' read -ra SPLIT <<< "$BUILD"
+    OS=${SPLIT[0]}
+    ARCH=${SPLIT[1]}
+
+    echo "Building ${OS}_$ARCH..."
+    (env GOOS=$OS GOARCH=$ARCH \
+        go build \
+            -o ${BUILDPATH}/${BUILDNAME}_${OS}_$ARCH \
+            -ldflags " \
+                -X ${PKGNAME}/ldflags.AppVersion=$TAG \
+                -X ${PKGNAME}/ldflags.AppCommit=$COMMIT \
+                -X ${PKGNAME}/ldflags.Release=TRUE" \
+                ./cmd/server
+    )
+            
+
+    if [ "$OS" = "windows" ]; then
+        mv ${BUILDPATH}/${BUILDNAME}_windows_$ARCH $BUILDPATH/${BUILDNAME}_windows_${ARCH}.exe
+    fi
+
+done
